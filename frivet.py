e4fd644b98ea97f5a788c18ba31d8ba89d3dc531
Solomon de Boissay <112638122+smdb1@users.noreply.github.com>
2025-12-16 15:30:13 +0000

    Update requirements for testing

--- frivet.py
+++ frivet.py
@@ -1,12 +1,7 @@
 from bottle import Bottle
 
-frivet  = Bottle()
+frivet = Bottle()
 
 @frivet.route("/")
 def index():
     return "<h1>Frivet: Prototype</h1>"
-
-frivet.run(
-    host="localhost",
-    port=8080
-)
